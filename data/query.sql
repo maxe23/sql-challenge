ddc63d86ecce85932facffb3191a83b0b061fd6b
maxe23 <maxeiteljorg@gmail.com>
2021-03-31 23:17:41 -0400

    decending frequency count of employees last name

--- data/query.sql
+++ data/query.sql
@@ -39,3 +39,9 @@ INNER JOIN employees
 ON dept_emp.emp_no = employees.emp_no
 WHERE departments.dept_name = 'Sales' OR departments.dept_name ='Development'
 ORDER BY emp_no;
+
+--In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+SELECT last_name, COUNT (employees.last_name)
+FROM employees
+GROUP BY last_name
+ORDER BY last_name DESC;
