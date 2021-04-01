c5d529f881f6ed7a21bed3bad0d85aeff734fa6f
maxe23 <maxeiteljorg@gmail.com>
2021-03-31 23:12:06 -0400

    2nd query hercules and B last names

--- data/query.sql
+++ data/query.sql
@@ -8,3 +8,8 @@ ON dept_emp.dept_no = departments.dept_no
 INNER JOIN employees
 ON dept_emp.emp_no = employees.emp_no
 ORDER BY emp_no;
+
+--List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+SELECT first_name, last_name, sex
+FROM employees
+WHERE first_name = 'Hercules' AND last_name LIKE 'B%';
