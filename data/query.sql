3d95a4a21f6830db6c61020b9b34d7762dd04d39
maxe23 <maxeiteljorg@gmail.com>
2021-03-31 23:16:43 -0400

    employees in sales and ev

--- data/query.sql
+++ data/query.sql
@@ -26,3 +26,16 @@ INNER JOIN employees
 ON dept_emp.emp_no = employees.emp_no
 WHERE departments.dept_name = 'Sales'
 ORDER BY emp_no;
+
+--List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+SELECT dept_emp.emp_no,
+employees.last_name,
+employees.first_name,
+departments.dept_name
+FROM dept_emp
+INNER JOIN departments
+ON dept_emp.dept_no = departments.dept_no
+INNER JOIN employees
+ON dept_emp.emp_no = employees.emp_no
+WHERE departments.dept_name = 'Sales' OR departments.dept_name ='Development'
+ORDER BY emp_no;
