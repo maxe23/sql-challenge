e93159237f958005f08469b373e3594e9f12cb0b
maxe23 <maxeiteljorg@gmail.com>
2021-03-31 23:13:01 -0400

    all employess in sales

--- data/query.sql
+++ data/query.sql
@@ -13,3 +13,16 @@ ORDER BY emp_no;
 SELECT first_name, last_name, sex
 FROM employees
 WHERE first_name = 'Hercules' AND last_name LIKE 'B%';
+
+--List all employees in the Sales department, including their employee number, last name, first name, and department name.
+SELECT dept_emp.emp_no,
+employees.last_name,
+employees.first_name,
+departments.dept_name
+FROM dept_emp
+INNER JOIN departments
+ON dept_emp.dept_no = departments.dept_no
+INNER JOIN employees
+ON dept_emp.emp_no = employees.emp_no
+WHERE departments.dept_name = 'Sales'
+ORDER BY emp_no;
